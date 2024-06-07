3ff47c013f4107e8356f360d33ecaeeb5dac17bc
AISHWARYA MOURYA <0712aishwarya@gmail.com>
2024-06-07 16:16:04 +0530

    first

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+      Turn Life's Moments into Memories:
+      Life throws amazing celebrations your way – birthdays, anniversaries, game nights bursting with laughter, pool parties under the summer sun, and even the most magical weddings. But planning them can feel overwhelming.
+
+That's where we come in.  We're your one-stop shop for transforming any event into an unforgettable experience. Our expert planners handle everything, from vendor selection and budget management to venue bookings and logistics. We listen to your vision and translate it into stunning décor, personalized touches, and activities that keep everyone engaged.
+
+Let go of the stress and embrace the joy.  Get a free quote today and start creating memories that will last a lifetime.
+      </p>
+    </section>
+  );
+};
+
+export default About;
